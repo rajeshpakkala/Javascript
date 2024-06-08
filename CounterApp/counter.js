32297c784c568ea2cc5b51dd31933a62670f4427
rajeshpakkala <rajeshpakkala786@gmail.com>
2024-06-08 13:59:03 +0530

    button colors added

--- CounterApp/counter.js
+++ CounterApp/counter.js
@@ -3,16 +3,39 @@ let counterElement=document.getElementById("countervalue");
 function onIncrement(){
         let prevvalue=counterElement.textContent;
         let updatevalue=parseInt(prevvalue)+1;  
-        counterElement.textContent=updatevalue;
-       
+     
+       if(updatevalue>0){
+        counterElement.style.color="green";
+       }
+       else if(updatevalue<0){
+        counterElement.style.color="red";
+       }
+       else{
+        counterElement.style.color="black"
+       }
+       counterElement.textContent=updatevalue;
 
 }
 function onDecrement(){
         let prevvalue=counterElement.textContent;
         let updatevalue=parseInt(prevvalue)-1;  
-        counterElement.textContent=updatevalue;
+        
+        if(updatevalue>0){
+                counterElement.style.color="green";
+               }
+        else if(updatevalue<0){
+                counterElement.style.color="red";
+               }
+        else{
+                counterElement.style.color="black"
+               }
+               counterElement.textContent=updatevalue;
 }
 function onReset(){
     let countervalue=0;
     counterElement.textContent=countervalue;
+      
+        counterElement.style.color="black"
+       
+      
 }
